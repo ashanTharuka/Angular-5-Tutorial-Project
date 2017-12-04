42928f1da0f4593ce753ce0fa73fd27694bb9e33
ahsan <ashantharuka371@outlook.com>
2017-12-04 22:05:11 +0530

    http client done1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,43 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-
-import { AppRoutingModule }     from './app-routing.module';
-
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
-import { HeroService }          from './hero.service';
-import { MessageService }       from './message.service';
-import { MessagesComponent }    from './messages/messages.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HeroService} from '../app/service/hero.service';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './component/heroes/heroes.component';
+import {HeroDetailComponent} from './component/hero-detail/hero-detail.component';
+import {MessagesComponent} from './component/messages/messages.component';
+import {MessageService} from './service/message.service';
+import {AppRoutingModule} from './/app-routing.module';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './service/in-memory-data.service';
+import { HeroSearchComponent } from './component/hero-search/hero-search.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
+  ],
   imports: [
+    HttpClientInMemoryWebApiModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
+      InMemoryDataService, {dataEncapsulation: false}
     )
   ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent
-  ],
-  providers: [ HeroService, MessageService ],
-  bootstrap: [ AppComponent ]
+  providers: [HeroService, MessageService, InMemoryDataService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
